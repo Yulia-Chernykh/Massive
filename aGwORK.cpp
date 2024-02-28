7efc70dcfb23f9c332994ca6a2f1c11c002a0645
Yulia-Chernykh <ihavebeenworking@mail.ru>
2024-02-28 10:41:56 +0300

    8

--- aGwORK.cpp
+++ aGwORK.cpp
@@ -11,9 +11,10 @@ int main()
     for (int i = 0; i < 8; i++)
         A[i] = rand();
     cout << A[i];
-    for (int i=0; i<8;i++)
+    for (int i = 0; i < 8; i++)
         ereryeyreryrer
         rr24525wwwww
+        35566633;
     std::cout << "Hello World!\n";
 }
 
