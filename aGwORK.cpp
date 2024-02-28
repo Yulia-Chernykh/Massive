af30a3be44cb3e5720a30358b784423eef69bb77
Yulia-Chernykh <ihavebeenworking@mail.ru>
2024-02-28 10:36:06 +0300

    Добавьте файлы проекта.

---
+++ aGwORK.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    int A[8];
+    srand(time(0));
+    for (int i = 0; i < 8; i++)
+        A[i] = rand();
+    std::cout << "Hello World!\n";
+}
+
